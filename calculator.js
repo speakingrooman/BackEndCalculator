43f21be542765f660464002ce3b2e9bca0d0ef13
Rooman <Mohammad.zahid96@myhunter.cuny.edu>
2019-02-14 22:41:12 -0500

    added bmi calculator

--- calculator.js
+++ calculator.js
@@ -5,10 +5,10 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
-
 
 app.post("/", function(req, res) {
   var num1 = Number(req.body.num1);
@@ -31,6 +31,19 @@ app.post("/", function(req, res) {
     default:
   }
   res.send("The result of " + num1 + " " + op + " " + num2 + " = " + result);
+});
+
+
+
+app.get("/bmicalculator", function(req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res) {
+  var weight = Number(req.body.weight);
+  var height = Number(req.body.height);
+  var bmi = weight / Math.pow(height, 2);
+  res.send("Your BMI is " + bmi);
 });
 
 app.listen(3000, function() {
